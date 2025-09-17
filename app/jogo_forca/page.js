6191e03ffad5582c631e69875c99fe7109608e28
Guithentges <guithentges@gmail.com>
2025-09-17 16:36:51 -0300

    Estilizando css

--- app/jogo_forca/page.js
+++ app/jogo_forca/page.js
@@ -34,14 +34,14 @@ export default function JogoForca() {
         if (prev.includes(letter)) return prev;
         const novos = [...prev, letter];
         const venceu = palavra.split("").every(l => novos.includes(l));
-        if (venceu) setResultado(`Parabéns, você acertou a palavra: ${palavra}`);
+        if (venceu) setResultado(`Parabéns, você acertou a palavra: `);
         return novos;
       });
     } else {
       setErros(prev => {
         if (prev.includes(letter)) return prev;
         const novos = [...prev, letter];
-        if (novos.length >= 6) setResultado(`Você perdeu! A palavra era: ${palavra}`);
+        if (novos.length >= 6) setResultado(`Você perdeu! A palavra era: `);
         return novos;
       });
     }
@@ -70,47 +70,51 @@ export default function JogoForca() {
   }
 
   return (
-    <div className="jogo-forca-container">
-      <div><a href="./">Voltar para o Portfólio</a></div>
-      <h1>Jogo da Forca</h1>
-      <p>Bem-vindo ao Jogo da Forca! Tente adivinhar a palavra antes que a forca esteja completa.</p>
-      <div className="forca">
-        <div className="poste"></div>
-        <div className="barra-horizontal"></div>
-        <div className="corda"></div>
-        <div className="boneco">
-          { erros.length > 0 &&(<div className="cabeca"></div>)}
-          { erros.length > 1 &&(<div className="corpo"></div>)}
-          { erros.length > 2 &&(<div className="braco-esquerdo"></div>)}
-          { erros.length > 3 &&(<div className="braco-direito"></div>)}
-          { erros.length > 4 &&(<div className="perna-esquerda"></div>)}
-          { erros.length > 5 &&(<div className="perna-direita"></div>)}
-        </div>
+    <div className="forca-main">
+      <a className="voltar" href="./">Voltar para o Portfólio</a>
+      <div className="forca-header">
+        <h1>Jogo da Forca</h1>
+        <p>Bem-vindo ao Jogo da Forca! Tente adivinhar a palavra antes que a forca esteja completa.</p>
       </div>
-      <div className="palavra-chave">{mostrarPalavra()}</div>
-      {erros.length != 0 && (<div className="erros">Letras erradas: {erros.join(", ")}</div>)}
-
-      {!resultado && (
-        <div className="teclado">
-          {alfabeto.map((l) => {
-            const jaTentada = acertos.includes(l) || erros.includes(l);
-            return (
-              <button
-                key={l}
-                onClick={() => processLetter(l)}
-                disabled={jaTentada}
-                className={`tecla ${jaTentada ? "desativada" : ""}`}
-              >
-                {l}
-              </button>
-            );
-          })}
+      <div className="forca-container">
+        <div className="forca">
+          <div className="poste"></div>
+          <div className="barra-horizontal"></div>
+          <div className="corda"></div>
+          <div className="boneco">
+            { erros.length > 0 &&(<div className="cabeca"></div>)}
+            { erros.length > 1 &&(<div className="corpo"></div>)}
+            { erros.length > 2 &&(<div className="braco-esquerdo"></div>)}
+            { erros.length > 3 &&(<div className="braco-direito"></div>)}
+            { erros.length > 4 &&(<div className="perna-esquerda"></div>)}
+            { erros.length > 5 &&(<div className="perna-direita"></div>)}
+          </div>
         </div>
-      )}
+        <div className="palavra-chave">{mostrarPalavra()}</div>
+        {erros.length != 0 && (<div className="erros">Letras erradas: <span>{erros.join(", ")}</span></div>)}
 
-      {resultado && <div className="mensagem">{resultado}</div>}
+        {!resultado && (
+          <div className="teclado">
+            {alfabeto.map((l) => {
+              const jaTentada = acertos.includes(l) || erros.includes(l);
+              return (
+                <button
+                  key={l}
+                  onClick={() => processLetter(l)}
+                  disabled={jaTentada}
+                  className={`tecla ${jaTentada ? "desativada" : ""}`}
+                >
+                  {l}
+                </button>
+              );
+            })}
+          </div>
+        )}
 
-      <button onClick={reset}>Resetar Jogo</button>
+        {resultado && <div className="mensagem">{resultado} <span>{palavra}</span></div>}
+
+        <button className="reset" onClick={reset}>Resetar Jogo</button>
+      </div>
     </div>
   );
 }
